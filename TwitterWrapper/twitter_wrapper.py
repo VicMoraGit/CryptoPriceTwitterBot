085a45b97a899bd9eec62721378b79ceecd492f7
VicMoraGit <56488721+VicMoraGit@users.noreply.github.com>
2022-07-28 12:25:52 +0200

    TwitterWrapper class created

---
+++ TwitterWrapper/twitter_wrapper.py
@@ -0,0 +1,14 @@
+"""
+    Wraps Twitter's API to be used as an Object.
+
+"""
+class Twitter():
+    """
+    Twitter Object that eases Twitter API usage
+
+    Attributes:
+        -
+    """
+
+    def __init__(self) -> None:
+        pass
--- main.py
+++ main.py
@@ -5,6 +5,3 @@
 
 if __name__ == "__main__":
     pass
-
-
-
