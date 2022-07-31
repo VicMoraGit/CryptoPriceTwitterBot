5ced208870fef8cdf3000896db660758f30b0f57
VicMoraGit <56488721+VicMoraGit@users.noreply.github.com>
2022-07-31 13:22:35 +0200

    get price implemented

--- CoinMarketCapWrapper/CMCWrapper.py
+++ CoinMarketCapWrapper/CMCWrapper.py
@@ -2,7 +2,30 @@
     Wrapper for CoinMarketCap API
 """
 
+#standard imports 
+
+#package imports
+from requests import get
 class CMC():
+    """
+        CoinMarketCap API wrapper
+
+        Attributes:
+        - api_key = Your CMC API Key
+
+    """
     
     def __init__(self,api_key:str):
         self.api_key = api_key
+
+    def get_coin_price(self, coin_symbol:str):
+        headers = {"X-CMC_PRO_API_KEY":self.api_key}
+
+        url = "https://pro-api.coinmarketcap.com/"
+        endpoint = "v2/cryptocurrency/quotes/latest"
+        query = f"?symbol={coin_symbol}"
+        complete_url = url+endpoint+query
+
+        response = get(complete_url,headers=headers)
+        price = response.json()["data"][coin_symbol][0]["quote"]["USD"]["price"]
+        return price
