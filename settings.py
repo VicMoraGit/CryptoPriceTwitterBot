b1efd593fa39f91943c495af7af51483249b1eab
VicMoraGit <56488721+VicMoraGit@users.noreply.github.com>
2022-07-31 14:00:24 +0200

    done

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 
 import json
 from os import path
-
+from CoinMarketCapWrapper.coin import Coin
 class Settings:
     """
         Imports, store and exports bot settings
@@ -12,7 +12,11 @@ class Settings:
 
     def __init__(self, config_file_path:str = None) -> None:
         self.is_first_run = None
+        self.coins = []
         self.config_file_path = config_file_path
+        
+        self.twitter_api_key = None
+        self.twitter_api_key_secret = None
 
     def save(self):
         """
@@ -47,8 +51,10 @@ class Settings:
         try:
             settings = self._config_file2json()
             self.is_first_run = settings["isFirstRun"]
-            
-
+            self.coins = self._get_coins_list(settings["coins"])
+            self.twitter_api_key =  settings["APIKey"]
+            self.twitter_api_key_secret = settings ["APIKeySecret"]
+        
         except KeyError:
 
             print("Wrong configuration file format.")
@@ -61,11 +67,23 @@ class Settings:
 
         return True
 
-    def _get_coins_list(self, jsonCoins:dict):
+    def _get_coins_list(self, json_coins:dict):
         coins = []
 
-        for coin in jsonCoins:
-            
+        for json_coin in json_coins:
+
+            symbol = json_coin["symbol"]
+            interval = json_coin["interval"]
+            last_price = json_coin["lastPricePosted"]
+            token = json_coin["twitterAccount"]["oauthToken"]
+            secret = json_coin["twitterAccount"]["oauthTokenSecret"]
+
+
+            coin = Coin(symbol, interval, last_price,token,secret)
+            coins.append(coin)
+
+        return coins
+
 
     def _config_file2json(self):
 
