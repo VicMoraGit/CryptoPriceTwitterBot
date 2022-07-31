94a9117318701ffa8767868fcf45912edeebf52f
VicMoraGit <56488721+VicMoraGit@users.noreply.github.com>
2022-07-31 17:44:49 +0200

    price tweet condition fixed

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ if __name__ == "__main__":
 
     if not IS_LOADED:
         raise SettingsNotLoadedException()
-    
-    
+
+
     #Initialize wrappers
 
     tw = Twitter(settings.twitter_api_key,settings.twitter_api_key_secret)
@@ -52,30 +52,31 @@ if __name__ == "__main__":
     #Iterate over every coin account
 
     for coin in settings.coins:
-        
+
         #Price comparison
 
         currentPrice = round(cmc.get_coin_price(coin.symbol),2)
         currentPriceIntervalRounded = myfloor(currentPrice,coin.interval)
 
         lastPrice = coin.last_price
+        print(lastPrice)
+        print(currentPriceIntervalRounded)
 
-        #If prices are different checks whether it increased or decreased
+        #If price is less than last interval it checks if it reached the next lower interval
+        #If price is higuer than last interval it checks if it reached 
+        # the next higher interval (using floor we get rid of this)
         #And posts a tweet (after authenticating to twitter)
-        if lastPrice != currentPriceIntervalRounded:
-            
-            isBiggerThanLast = currentPriceIntervalRounded>lastPrice
+        if (lastPrice > currentPriceIntervalRounded and
+        lastPrice-coin.interval >= currentPrice) or lastPrice < currentPriceIntervalRounded:
+            is_bigger_than_last =lastPrice<currentPrice
 
             #Twitter authentication
-            tw.auth(coin.token, coin.secret)
 
+            tw.auth(coin.token, coin.secret)
             #Posts tweet based on price info
-            tw.post_tweet(currentPrice, isBiggerThanLast, coin.symbol)
+            tw.post_tweet(currentPrice, is_bigger_than_last, coin.symbol)
 
             #Saves the new price in the personal settings
             coin.last_price = currentPriceIntervalRounded
 
             settings.save()
-
-
-
